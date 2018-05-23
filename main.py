526d3528f7834380e275a2e492b362df5350bc08
S10rhub <lionkill1ger@gmail.com>
2018-05-23 15:33:39 +0200

    added basic argparse code

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import midi_parser.save_to_file as stf
 
 def main():
 
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    
+    # e.g.
+    # parser.add_argument("-l", "--logfile", required=False, help="Set the path and name of the log file", default="./output/ma_log.log")
+
+    args = parser.parse_args()
+
     print("TODO!")
 
 
